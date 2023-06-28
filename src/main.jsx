bdf9a653e111a0278c2a13a7d764e2c718d4e1ed
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2023-06-28 10:54:09 -0300

    verificando erro no deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './pages/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
 )
