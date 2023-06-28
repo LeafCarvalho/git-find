d3c1996da719d68de3a96276726beaa3bdc293b0
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2023-06-28 11:08:54 -0300

    realizando testes para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: "/git-find",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
-  server: {
-    mimeTypes: {
-      'application/javascript': ['js', 'mjs', 'jsx'], // Adicione 'jsx' aqui
-    },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
   },
 })
