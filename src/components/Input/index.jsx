aafdec9566b346e973c56526bc1645bdf7312019
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2023-07-03 18:59:44 -0300

    deixando placeholder mais nítido

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,7 +3,7 @@ import styles from './input.module.css';
 const Input = ({ nameValue, repoValue, onNameChange, onRepoChange }) => {
   return (
     <div className={styles.inputcontainer}>
-      <input className={styles.input} placeholder="Digite o nome" value={nameValue || ''} onChange={onNameChange} />
+      <input className={styles.input} placeholder="Digite o nome do usuário" value={nameValue || ''} onChange={onNameChange} />
       <input className={styles.input} placeholder="Digite o repositório" value={repoValue || ''} onChange={onRepoChange} />
     </div>
   );
