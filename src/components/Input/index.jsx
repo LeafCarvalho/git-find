5ba08299807f9581379d62a6c433e77d7fccefe9
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2023-07-03 18:59:05 -0300

    adicionando uma interface melhor com dois inputs

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,12 @@
-import styles from './input.module.css'
+import styles from './input.module.css';
 
-const Input = ({value, onChange}) => {
+const Input = ({ nameValue, repoValue, onNameChange, onRepoChange }) => {
   return (
     <div className={styles.inputcontainer}>
-          <input className={styles.input} placeholder='Digite o nome e repositório' value={value || ''} onChange={onChange}/>
+      <input className={styles.input} placeholder="Digite o nome" value={nameValue || ''} onChange={onNameChange} />
+      <input className={styles.input} placeholder="Digite o repositório" value={repoValue || ''} onChange={onRepoChange} />
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,46 +2,53 @@ import { useState } from "react";
 import '../reset.css';
 import Header from "../components/Header";
 import Input from "../components/Input";
-import api from '../services/api'
+import api from '../services/api';
 import Button from "../components/Button";
 import Repositorio from "../components/Repositorios";
 import { useOnKeyPress } from "../components/Button/onkeypress";
 
 export default function App() {
-  const [currentRepo, setCurrentRepo] = useState()
+  const [currentName, setCurrentName] = useState('');
+  const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const getRepositories = async () => {
-    const { data } = await api
-        .get(`repos/${currentRepo}`)
-        .catch((error) => {
-            alert("O repositório não foi localizado!");
-        });
+    const fullName = `${currentName}${currentRepo ? `/${currentRepo}` : ''}`;
+    const { data } = await api.get(`repos/${fullName}`).catch((error) => {
+      alert("O repositório não foi localizado!");
+    });
 
     if (data.id) {
-        const exists = repos.find((repo) => repo.id === data.id);
-        if (!exists) {
-            setRepos((prev) => [...prev, data]);
-            setCurrentRepo("");
-            return;
-        }
-        alert("O repositório já foi localizado!");
+      const exists = repos.find((repo) => repo.id === data.id);
+      if (!exists) {
+        setRepos((prev) => [...prev, data]);
+        setCurrentName('');
+        setCurrentRepo('');
+        return;
+      }
+      alert("O repositório já foi localizado!");
     }
-};
+  };
 
-useOnKeyPress(getRepositories, 'Enter')
+  useOnKeyPress(getRepositories, 'Enter');
 
   const removeRepositories = (id) => {
     setRepos(repos.filter((repo) => repo.id !== id));
-  }
+  };
 
-          return (
-            <>
-            <Header />
-            <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
-            <Button  onClick={getRepositories} />
-            {repos.map(repo => <Repositorio key={repo.id} repo={repo} remover={removeRepositories}/>)}
-            </>
-          );
+  return (
+    <>
+      <Header />
+      <Input
+        nameValue={currentName}
+        repoValue={currentRepo}
+        onNameChange={(e) => setCurrentName(e.target.value)}
+        onRepoChange={(e) => setCurrentRepo(e.target.value)}
+      />
+      <Button onClick={getRepositories} />
+      {repos.map((repo) => (
+        <Repositorio key={repo.id} repo={repo} remover={removeRepositories} />
+      ))}
+    </>
+  );
 }
